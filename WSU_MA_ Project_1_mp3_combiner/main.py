d544726cdc4f151166f757e89c050ce5bd03a2f9
Hitchhiker-Dave <mrhitchhiker42@gmail.com>
2024-02-20 00:21:24 -0800

    Full Sound Merging
    Can now input any 2 files in any audio format and merge them into one object.
    
    To do:
    find way to save new audio object as a sound file (worst case scenario I can just record my desktop in audacity)

--- WSU_MA_ Project_1_mp3_combiner/main.py
+++ WSU_MA_ Project_1_mp3_combiner/main.py
@@ -6,25 +6,36 @@ import pygame
 import random
 from time import sleep
 
-fs = 8000 # Hz, affects audio quality somewhat
+fs = 16000 # Hz, affects audio quality somewhat and sound durration calculations; also the bigger it is the longer it takes to sort through the array
 
 pygame.mixer.pre_init(fs, size=-16, channels=1) #not sure what size effects
 pygame.mixer.init()
 
-effect = pygame.mixer.Sound("guitar rift 1.wav")
-effect_digitized = pygame.sndarray.array(effect) #turn sound file into a valid pygame sound array
+effect1 = pygame.mixer.Sound("Darren Korb - Transistor- Original Soundtrack - 19 Tangent.mp3")
+effect2 = pygame.mixer.Sound("Darren Korb - Transistor- Original Soundtrack - 14 Apex Beat.mp3")
+
+effect_digitized1 = pygame.sndarray.array(effect1) #turn sound file into a valid pygame sound array
+effect_digitized2 = pygame.sndarray.array(effect2) #turn sound file into a valid pygame sound array
 #once the effect is digitized I can start messing around with the arrays
 
-#Effect distortion goes here
-'''for i in range(len(effect_digitized)):
-    effect_digitized[i] = effect_digitized[i] * random.uniform(-1, 1)'''
+array_length = 0 #only update array up to the smallest sound file's index
+if len(effect_digitized1) <= len(effect_digitized2):
+    array_length = len(effect_digitized1)
+else:
+    array_length = len(effect_digitized2)
 
-print(len(effect_digitized))
-song_length = (len(effect_digitized)) / 10000 # Figure out how to find song length, either that or have a way to detect the way the song is playing and 
-                                                #have the program sleep until the song is over
+#Effect distortion goes here; never multiply the value by more then one to prevent hearing damage; maybe multiply the result by 0.x for safety
+for i in range(array_length):
+    average = (effect_digitized1[i][0] + effect_digitized1[i][1] + effect_digitized2[i][0] + effect_digitized2[i][1]) *0.25
+    effect_digitized1[i][0] = average
+    effect_digitized1[i][1] = average
 
-sound = pygame.sndarray.make_sound(effect_digitized) #use sound array to play a sound
+
+song_length = (len(effect_digitized1)) / fs #divide the length of the array by the frequency to always get the correct length
+
+sound = pygame.sndarray.make_sound(effect_digitized1) #use sound array to play a sound
 
 sound.play()
 
 sleep(song_length) # NOTE: Since sound playback is async, allow sound playback to start before Python exits
+print("end")
