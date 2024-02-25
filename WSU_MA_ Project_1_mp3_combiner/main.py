4f853b604b8e043959e818e7c44cba9af9eeba41
Hitchhiker-Dave <112595100+Hitchhiker-Dave@users.noreply.github.com>
2024-02-25 02:43:45 -0800

    Cleanup
    generalized variables so that it wasn't dependent on editing the original sound file

--- WSU_MA_ Project_1_mp3_combiner/main.py
+++ WSU_MA_ Project_1_mp3_combiner/main.py
@@ -11,29 +11,36 @@ fs = 16000 # Hz, affects audio quality somewhat and sound durration calculations
 pygame.mixer.pre_init(fs, size=-16, channels=1) #not sure what size effects
 pygame.mixer.init()
 
-effect1 = pygame.mixer.Sound("Darren Korb - Transistor- Original Soundtrack - 19 Tangent.mp3")
+effect1 = pygame.mixer.Sound("Fear Hunger OST Pulse and Anxiety.mp3")
 effect2 = pygame.mixer.Sound("Darren Korb - Transistor- Original Soundtrack - 14 Apex Beat.mp3")
 
 effect_digitized1 = pygame.sndarray.array(effect1) #turn sound file into a valid pygame sound array
 effect_digitized2 = pygame.sndarray.array(effect2) #turn sound file into a valid pygame sound array
-#once the effect is digitized I can start messing around with the arrays
+new_digitized_effect = None
+#once the effect is digitized I can start messing around with the arrays``
 
 array_length = 0 #only update array up to the smallest sound file's index
 if len(effect_digitized1) <= len(effect_digitized2):
-    array_length = len(effect_digitized1)
+    new_digitized_effect = effect_digitized1
 else:
-    array_length = len(effect_digitized2)
+    new_digitized_effect = effect_digitized2
+
+array_length = len(new_digitized_effect)
 
 #Effect distortion goes here; never multiply the value by more then one to prevent hearing damage; maybe multiply the result by 0.x for safety
+print("Compiling Song")
 for i in range(array_length):
     average = (effect_digitized1[i][0] + effect_digitized1[i][1] + effect_digitized2[i][0] + effect_digitized2[i][1]) *0.25
-    effect_digitized1[i][0] = average
-    effect_digitized1[i][1] = average
+    new_digitized_effect[i][0] = average * 0.5
+    new_digitized_effect[i][1] = average * 0.5
+    
 
 
 song_length = (len(effect_digitized1)) / fs #divide the length of the array by the frequency to always get the correct length
 
-sound = pygame.sndarray.make_sound(effect_digitized1) #use sound array to play a sound
+sound = pygame.sndarray.make_sound(new_digitized_effect) #use sound array to play a sound
+
+print("Song Compiled")
 
 sound.play()
 
